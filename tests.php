d4fbd727fbd2280766b8a7514f14f910a679de5d
Linards Lazdiņš <linards_lazdins@hotmail.com>
2018-04-10 16:54:26 -0400

    test page mockup

---
+++ tests.php
@@ -0,0 +1,44 @@
+<?php include_once 'includes/header.php' ?>
+
+<div class="container">
+    <div class="row">
+        <h4>Sveiki, [vārds]!</h4>
+        <h2 class="text-center">[jautājums]</h2>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="btn btn-default">
+                Atbilde 1
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="btn btn-default">
+                Atbilde 2
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="btn btn-default">
+                Atbilde 3
+            </div>
+        </div>
+    
+        <div class="col-md-6">
+            <div class="btn btn-default">
+                Atbilde 4
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+            <span class="sr-only">60% Complete</span>
+        </div>
+    </div>
+</div>
+
+<?php include_once 'includes/footer.php' ?>
