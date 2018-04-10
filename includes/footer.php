eabad41f0f3762f9131e185d7aecf24010176d42
Linards Lazdiņš <linards_lazdins@hotmail.com>
2018-04-10 16:42:38 -0400

    Add includes with header and footer

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+
+    <!-- JavaSript -->
+    <!-- jQuery -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Custom JS -->
+    <script src="js/script.js"></script>
+
+    <!-- FONT-AWESOME  -->
+    <script src="js/fontawesome-all.min.js"></script>
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"> -->
+    <title>Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Google fonts -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Dosis:400,700" rel="stylesheet"> -->
+
+</head>
+
+
+<body>
